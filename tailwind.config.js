cccdf9811bccdc35d9ac668e70a1d79278dda64f
vorillaz <theodorevorillas@gmail.com>
2024-07-01 16:49:23 +0300

    before moving to videostack

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     colors: {
+      "media-brand": "rgb(var(--media-brand) / <alpha-value>)",
+      "media-focus": "rgb(var(--media-focus) / <alpha-value>)",
       transparent: "transparent",
       white: "#ffffff",
       black: "#000000",
@@ -11,5 +16,19 @@ module.exports = {
       noop: "rgba(51, 51, 51, 0.5019607843)",
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@vidstack/react/tailwind.cjs")({
+      prefix: "media",
+    }),
+    customVariants,
+  ],
 };
+
+function customVariants({ addVariant, matchVariant }) {
+  // Strict version of `.group` to help with nesting.
+  matchVariant("parent-data", (value) => `.parent[data-${value}] > &`);
+
+  addVariant("hocus", ["&:hover", "&:focus-visible"]);
+  addVariant("group-hocus", [".group:hover &", ".group:focus-visible &"]);
+}
